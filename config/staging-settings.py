3e8761792c4b516fb85e932ca0c7311f3eae82cc
Habeeb Shopeju <shopejuh@gmail.com>
2020-03-26 12:50:47 +0100

    Changed deployment destination

--- config/staging-settings.py
+++ config/staging-settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['healfb.herokuapp.com']
+ALLOWED_HOSTS = ['healfb.herokuapp.com', 'curefb.herokuapp.com']
 
 
 # Application definition
